8f6e3e62b241ef7cb05c51686b9026e4447f5c11
achatt89 <admin@abhijitchatterjee.com>
2015-08-04 00:11:19 +0530

    Create User - CSS Changes, Added Error/Success Messages, Disabled Submit Button untill valid data in the form

--- app/scripts/controller/createUserCtrl.js
+++ app/scripts/controller/createUserCtrl.js
@@ -3,19 +3,42 @@ angular.module('starter')
     .controller('createUserCtrl', function(constantCreateUserURL, $scope, $firebaseAuth) {
         $scope.createUser = {};
 
-        $scope.addUser = function() {
-            var ref = new Firebase(constantCreateUserURL);
-			$scope.authObj = $firebaseAuth(ref);
+        $scope.createUserSuccess = false;
+        $scope.passwordError = false;
+        $scope.userExists = false;
+        $scope.createUserForm = {};
 
-            $scope.authObj.$createUser({
-                email: $scope.createUser.userName,
-                password: $scope.createUser.password
-            })
-            .then (function(userData) {
-            	console.log("User Created Successfully", userData);
-            })
-            .catch (function(error) {
-            	console.log("User Creating Error", error);
-            })
+        $scope.addUser = function() {
+
+            if ($scope.createUser.password === $scope.createUser.confirmPassword) {
+                var ref = new Firebase(constantCreateUserURL);
+                $scope.authObj = $firebaseAuth(ref);
+
+                $scope.authObj.$createUser({
+                        email: $scope.createUser.userName,
+                        password: $scope.createUser.password
+                    })
+                    .then(function(userData) {
+                        console.log("User Created Successfully", userData);
+                        $scope.userExists = false;
+                        $scope.passwordError = false;
+                        $scope.createUserSuccess = true;
+
+                        console.log($scope.createUserForm);
+
+                        //$scope.createUserForm.$setPristine();
+
+                    })
+                    .catch(function(error) {
+                        console.log("User Creating Error", error);
+                        $scope.passwordError = false;
+                        $scope.createUserSuccess = false;
+                        $scope.userExists = true;
+                    })
+            } else {
+                $scope.createUserSuccess = false;
+                $scope.userExists = false;
+                $scope.passwordError = true;
+            }
         };
     });
