fe7751a4b7b8432e258bded290c6b41410133e24
achatt89 <admin@abhijitchatterjee.com>
2015-08-11 16:19:13 +0530

    Changed UI for Calendar & Salary Details; Added New Validation templates for Salary Details; Added Validation to SalaryDetailsCtrl

--- app/scripts/controller/salaryDetailsCtrl.js
+++ app/scripts/controller/salaryDetailsCtrl.js
@@ -16,24 +16,49 @@ angular.module('expenseTracker')
 
             $scope.addSalaryDetailsToDB = function() {
 
-                for (var i = 0; i < $scope.bankDetails.length; i++) {
-                    if ($scope.salaryDetails.accountNumber === $scope.bankDetails[i].accountNumber) {
-                        var id = $scope.bankDetails[i].$id;
+                checkInput();
 
-                        var updateItem = $scope.bankDetails.$getRecord(id);
+                if ($scope.salaryDetailsError === false) {
+                    for (var i = 0; i < $scope.bankDetails.length; i++) {
+                        if ($scope.salaryDetails.accountNumber === $scope.bankDetails[i].accountNumber) {
+                            var id = $scope.bankDetails[i].$id;
 
-                        if (updateItem !== null || updateItem !== undefined || updateItem !== '') {
-                            updateItem.currentAmount = parseFloat(updateItem.currentAmount) + parseFloat($scope.salaryDetails.salaryAmount); // updateItem.currentAmount -----> currentAmount is the field in JSON
+                            var updateItem = $scope.bankDetails.$getRecord(id);
 
-                            $scope.bankDetails.$save(updateItem);
-                            $scope.salaryDetailsError = false;
-                            break;
-                        } else {
-                            $scope.salaryDetailsError = true;
+                            if (updateItem !== null || updateItem !== undefined || updateItem !== '') {
+                                updateItem.currentAmount = parseFloat(updateItem.currentAmount) + parseFloat($scope.salaryDetails.salaryAmount); // updateItem.currentAmount -----> currentAmount is the field in JSON
+
+                                $scope.bankDetails.$save(updateItem);
+                                $scope.salaryDetailsError = false;
+
+                                $scope.salaryDetails.accountNumber = false;
+                                $scope.salaryDetails.salaryAmount = undefined;
+
+                                break;
+                            } else {
+                                $scope.salaryDetailsError = true;
+                            }
                         }
                     }
                 }
 
             };
+
+            var checkInput = function() {
+                if ($scope.salaryDetails.salaryAmount === undefined) {
+                    $scope.salaryAmountError = true;
+                    $scope.salaryDetailsError = true;
+                    $scope.accountNumberError = false;
+                } else if (!$scope.salaryDetails.accountNumber) {
+                    $scope.salaryAmountError = false;
+                    $scope.accountNumberError = true;
+                    $scope.salaryDetailsError = true;
+                }
+                else {
+                    $scope.accountNumberError = false;
+                    $scope.salaryAmountError = false;
+                    $scope.salaryDetailsError = false;
+                }
+            }
         }
     });
