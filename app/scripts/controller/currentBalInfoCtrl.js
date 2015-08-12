e715a46d6501813e7db0a77b67d645132cd07651
achatt89 <admin@abhijitchatterjee.com>
2015-08-12 13:14:21 +0530

    Added currentBalInfo Modal with complete bank details

--- app/scripts/controller/currentBalInfoCtrl.js
+++ app/scripts/controller/currentBalInfoCtrl.js
@@ -1,15 +1,37 @@
 'use strict';
 angular.module('expenseTracker')
-    .controller('currentBalInfoCtrl', function(localStorage, $state, commonCalls, $scope) {
+    .controller('currentBalInfoCtrl', function(localStorage, $state, commonCalls, $scope, $ionicModal) {
         if (localStorage.get('authenticationData') === 'undefined') {
             $state.go('app.login');
         } else {
             $scope.currentBalInfo = {};
+            $scope.modalBankInfo = {};
 
             $scope.currentDate = commonCalls.fetchCurrentDate();
             $scope.previousDate = commonCalls.fetchPreviousDate();
 
             $scope.currentBalInfo = commonCalls.bankDetailsFbData();
+
+            $ionicModal.fromTemplateUrl('../views/templates/modalTemplates/bankDetails.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
+
+            $scope.openModal = function(index) {
+                $scope.modal.show();
+
+                $scope.modalBankInfo = $scope.currentBalInfo[index];
+            };
+
+            $scope.closeModal = function() {
+                $scope.modal.hide();
+            };
+
+            $scope.$on('$destroy', function() {
+                $scope.modal.remove();
+            });
 
         }
 
